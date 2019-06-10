94cde01fc694c939337e23282643b71861144fb4
Camerone Stoney <Cameronestoney@yahoo.com>
2019-06-10 11:43:26 -0700

    WIP

--- assets/app.js
+++ assets/app.js
@@ -1,7 +1,24 @@
+
+console.log("working");
 $(document).ready(function () {
 
-    $('#sidebarCollapse').on('click', function () {
-        $('#sidebar').toggleClass('active');
-    });
+    
 
+    function openSlideMenu() {
+
+        document.getElementById('slide-open').style.width = '250px';
+        document.getElementById('side-menu').style.width = '250px'
+        document.getElementById('content-section').style.marginLeft = '250px';
+        console.log("open")
+    }
+    openSlideMenu();
+
+    function closeSlideMenu() {
+
+        document.getElementById('side-menu').style.width = '0';
+        document.getElementById('content-section').style.marginLeft = '0';
+
+    }
+
+    closeSlideMenu();
 });
